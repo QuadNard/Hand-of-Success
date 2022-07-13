496abf563b488d52add10b8b81825fe8c74ca7ff
QuadNard <tullochyorkcity@gmail.com>
2022-07-12 21:46:54 -0400

    Updated routes ~client

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
-import Home from './pages/Home';
-import Quote from './pages/Quote';
-import Todo from './pages/Todo';
-import Zulu from './pages/Zulu';
-import { Route, Routes  } from 'react-router-dom';
+
+
 
 
 function App() {
   return (
-    <div>
-      <Routes>
-         <Route path='/' element={<Home/>} />
-       <Route path='/quote' element={<Quote/>} />
-         <Route path='/todo' element={<Todo/>} />
-        <Route path='/zulu' element={<Zulu/>} />         
-      </Routes>
-    
+    <div className="App">
+       <h1>Home page</h1>  
+        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
+    Next
+     </button>
     </div>
 
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import Todo from './pages/Todo';
+import Zulu from './pages/Zulu';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+         <Route path='/' element={<App/>} />
+         <Route path='/home' element={<Home/>} />
+       <Route path='/quote' element={<Quote/>} />
+         <Route path='/todo' element={<Todo/>} />
+        <Route path='/zulu' element={<Zulu/>} />         
+      </Routes>
   </BrowserRouter>
 );
 
